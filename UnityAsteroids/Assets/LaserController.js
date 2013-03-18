aceea84bcba56124ced7208e3bff708945fdd215
unknown <chridegu@L135-12.mcast.org.mt>
2013-03-18 15:06:05 +0100

    PowerUp

---
+++ UnityAsteroids/Assets/LaserController.js
@@ -0,0 +1,34 @@
+#pragma strict
+var laserspeed:int;
+
+
+function Start () {
+
+}
+
+function Update () {
+	//all the laser needs is forward motion
+	transform.Translate(Vector3.up * laserspeed * Time.deltaTime);
+
+}
+
+//when the laser exits the scene
+function OnBecameInvisible()
+{
+	Destroy(this.gameObject);
+}
+
+function OnTriggerEnter(other:Collider)
+{
+	//is the other object an asteroid?
+	if(other.gameObject.tag == "asteroid")
+	{
+		
+		//destroy the laser
+		Destroy(this.gameObject);
+		//destroy asteroid
+		Destroy(other.gameObject);
+		//increment the score
+		PlayerController.score++;
+	}
+}
---
+++ UnityAsteroids/Assets/PlayerController.js
@@ -0,0 +1,96 @@
+#pragma strict
+var normalSpeed:int;
+var turboSpeed:int;
+
+//game is not over yet.
+var gameover:boolean=false;
+
+//this is the slot for the laser prefab
+var laserprefab:Rigidbody;
+
+//start time initialized to float and set to zero
+var startTime:float=0.0;
+var currentTime:float=0.0;
+var elapsedTime:int=0;
+static var score:int=0;
+static var health: int = 100;
+
+
+
+
+function Start () {
+	//the time the player started playing
+	startTime=Time.time;
+}
+
+function Update () {
+	currentTime = Time.time;
+	//il-hin li ghadda nahdmuh hekk:
+	
+	//kill the game after sixty seconds
+	if(health <=0)
+	{
+		gameover = true;
+	}
+	if (elapsedTime < 60)
+	{
+		elapsedTime = currentTime - startTime;
+		//enable borders using the borders function in BorderController
+		BorderController.EnableBorders(transform);
+
+		//rotation of the cube
+		transform.Rotate(Vector3.forward * -40 * Input.GetAxis("Horizontal") * Time.deltaTime);
+	
+		//fire is the left ctrl key
+		if(Input.GetKeyDown(KeyCode.LeftControl))
+		{
+			//create a laser at the position of the space ship.
+			Instantiate(laserprefab,transform.position,transform.rotation);
+		}
+	
+		//speed / turbo speed
+		if (Input.GetKey(KeyCode.Space))
+		{
+			transform.Translate(Vector3.up * turboSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+		}
+		else
+		{
+			//if not holding space bar
+			transform.Translate(Vector3.up * normalSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+		}
+		
+	} else {
+		//the game is over
+		gameover = true;
+		//game over
+		print("game over");
+	}
+
+}
+
+function OnGUI()
+{
+	GUI.Label(Rect(0,0,150,50),"Elapsed Time: "+elapsedTime);
+	GUI.Label(Rect(0,20,150,50),"Score: "+score);
+	GUI.Label(Rect(0,40,150,50),"Health: "+health);
+	
+	if (gameover == "true")
+	{
+	GUI.Label(Rect(100,100,250,50),"Game Over");
+	}
+}
+
+function OnTriggerEnter(other:Collider)
+{
+	if (other.gameObject.tag == "asteroid")
+	{
+		//reduce 1% health
+			health--;
+	}
+	if (gameover == true)
+	{
+	print ("Game Over");
+	}
+	
+
+}
